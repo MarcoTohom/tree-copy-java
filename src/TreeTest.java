ee5d21e502c613a3e7e1d6dd52af13c24fdff5dc
MarcoTohom <marcotohom@zoho.com>
2022-04-30 10:55:09 -0600

    Initial commit

---
+++ src/TreeTest.java
@@ -0,0 +1,39 @@
+import java.util.Optional;
+
+public class TreeTest {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.add(5);
+        root.add(15);
+        root.add(8);
+
+        Optional<Node> result = root.find(11);
+        if (result.isPresent()) {
+            System.out.println(result.get());
+        } else {
+            System.out.println("Value not found");
+        }
+
+        result = root.find(8);
+        if (result.isPresent()) {
+            System.out.println(result.get());
+        } else {
+            System.out.println("Value not found");
+        }
+        System.out.println("Print in order");
+        root.printInOrder();
+        System.out.println("Print pos order");
+        root.printPosOrder();
+        System.out.println("Print pre order");
+        root.printPreOrder();
+
+        Node nuevoNodo = new Node(root, root.getLeft(), root.getRight());
+        System.out.println("***************************************");
+        System.out.println("Print in order");
+        root.printInOrder();
+        System.out.println("Print pos order");
+        root.printPosOrder();
+        System.out.println("Print pre order");
+        root.printPreOrder();
+    }
+}
